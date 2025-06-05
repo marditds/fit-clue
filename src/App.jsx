be38be718a3ac6ab289b2659451c0b530e95fc65
marditds <marditds@gmail.com>
2025-06-04 20:28:01 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <p>Hakob</p>
+      </div>
+    </>
+  )
+}
+
+export default App
