02ce91ee92e535f825c84653349f311b63dc00e6
marditds <marditds@gmail.com>
2025-06-08 19:48:13 -0700

    Update main.js

--- functions/oembed/src/main.js
+++ functions/oembed/src/main.js
@@ -5,7 +5,7 @@ export default async ({ req, res, log, error }) => {
         const data = typeof req.body === 'string' ? JSON.parse(req.body) : req.body;
 
         log('data:', data);
-        log('data.data:', data.data);
+        log('data.hakobos:', data.hakobos);
 
         const url = `https://graph.facebook.com/v19.0/instagram_oembed?` +
             new URLSearchParams({
