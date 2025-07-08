35edaf98e3846c03f3ff71b35bafa9c3623bf42d
marditds <marditds@gmail.com>
2025-07-07 20:07:59 -0700

    Update main.js

--- functions/gemini/src/main.js
+++ functions/gemini/src/main.js
@@ -5,7 +5,7 @@ export default async ({ req, res, log, error }) => {
   const GeminiApiKey = process.env.GEMINI_API_KEY;
 
   try {
-    const data = req.body;
+    let data = typeof req.body === 'string' ? JSON.parse(req.body) : req.body;
 
     log('data:', data);
 
