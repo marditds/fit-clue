2f16f680f793e558041174a9d449e297277cf29f
marditds <marditds@gmail.com>
2026-04-09 17:46:26 -0700

    Update TOSData.jsx

--- src/components/Legal/TOSData.jsx
+++ src/components/Legal/TOSData.jsx
@@ -33,10 +33,10 @@ const TOSData = () => {
             description: (
                 <>
                     <p>
-                        FitClue is a personal project and not a product of a large or well-established organization. Users should not expect the same level of infrastructure, security, or features typically provided by major, well-established platforms.
+                        FitClue is a personal project and does not provide the level of infrastructure, security, or features typically offered by large, established platforms. You should not expect the same level of infrastructure, security, or features typically provided by major, well-established platforms.
                     </p>
                     <p>
-                        While every reasonable effort is made to ensure the reliability of the platform, FitClue cannot guarantee advanced security features or comprehensive protections against potential risks. Users are encouraged to use strong passwords and exercise caution when sharing sensitive information.
+                        While every reasonable effort is made to ensure the reliability of the platform, FitClue cannot guarantee advanced security features or comprehensive protections against potential risks. You are encouraged to use strong passwords and exercise caution when sharing sensitive information.
                     </p>
                 </>
             )
@@ -45,14 +45,14 @@ const TOSData = () => {
             title: 'Account Creation',
             description:
                 <p>
-                    Any valid email owner can create an account with FitClue. By registering, users agree to provide a valid email address, which is collected and stored through FitClue's backend infrastructure, powered by Appwrite. This email is used solely for account management and communication purposes.
+                    Any individual with a valid email address may create an account. By registering, you agree to provide a valid email address, which is stored through FitClue's backend infrastructure, powered by Appwrite. This email is used solely for account management and communication purposes.
                 </p>
         },
         {
             title: 'Security and Account Access',
             description:
                 <p>
-                    FitClue uses Google reCAPTCHA to prevent automated sign-ups during account creation. The platform does not provide additional security measures such as multi-factor authentication. Users are solely responsible for securing their accounts and are advised to use strong, unique passwords. FitClue is not liable for unauthorized access resulting from weak or compromised credentials.
+                    FitClue uses Google reCAPTCHA to prevent automated sign-ups during account creation. The platform does not provide additional security measures such as multi-factor authentication. You are solely responsible for securing your account and are advised to use strong, unique passwords. FitClue is not liable for unauthorized access resulting from weak or compromised credentials.
                 </p>
         },
         {
@@ -67,14 +67,14 @@ const TOSData = () => {
             title: 'Content Responsibility',
             description:
                 <p>
-                    By using FitClue, users accept full responsibility for any content they share on the platform. FitClue is not responsible for the accuracy, reliability, or legality of user-added content, including links. Users are solely responsible for ensuring their content complies with all applicable laws and does not infringe on the rights of others.
+                    By using FitClue, you accept full responsibility for any content you share on the platform. FitClue is not responsible for the accuracy, reliability, or legality of user-added content, including links. You are solely responsible for ensuring your content complies with all applicable laws and does not infringe on the rights of others.
                 </p>
         },
         {
             title: 'Exclusivity of Content',
             description: (
                 <p>
-                    FitClue does not guarantee or enforce the exclusivity of content shared by users. It is the user's responsibility to ensure they have the necessary rights and permissions to share any content on the platform.
+                    FitClue does not guarantee or enforce the exclusivity of content shared by you. It is your responsibility to ensure that you have the necessary rights and permissions to share any content on the platform.
                 </p>
             )
         },
@@ -90,7 +90,7 @@ const TOSData = () => {
             title: 'Link Management',
             description: (
                 <p>
-                    FitClue reserves the right to update, modify, or remove any links added by users at any time, with or without prior notice. Users acknowledge that the platform is under no obligation to notify them about such changes or provide any explanation or justification for them.
+                    FitClue reserves the right to update, modify, or remove any links added by you at any time, with or without prior notice. You acknowledge that the platform is under no obligation to notify you about such changes or provide any explanation or justification for them.
                 </p>
             )
         },
@@ -125,7 +125,7 @@ const TOSData = () => {
                 <>
                     <p>
                         FitClue is provided on an 'as is' and 'as available' basis without warranties of any kind, either express or implied.
-                        To the fullest extent permitted by law, FitClue and its creator(s) disclaim all liability for any damages, losses, or claims arising from your access to or use of the platform; any content posted by you or other users; third-party services or integrations (e.g., Appwrite, Google Gemini, Instagram); downtime, data loss, or service interruptions; and unauthorized access to your account due to weak credentials or user negligence.
+                        To the fullest extent permitted by law, FitClue and its creator(s) disclaim all liability for any damages, losses, or claims arising from your access to or use of the platform; any content posted by you or other users; third-party services or integrations (e.g., Appwrite, Google Gemini, Instagram); downtime, data loss, or service interruptions; and unauthorized access to your account due to weak credentials or negligence.
                     </p>
                     <p>
                         By using FitClue, you acknowledge and agree that you assume full responsibility for your use of the platform. Under no circumstances shall FitClue or its creator(s) be liable for any indirect, incidental, consequential, special, or punitive damages, even if advised of the possibility of such damages.
@@ -138,10 +138,10 @@ const TOSData = () => {
             description: (
                 <>
                     <p>
-                        FitClue reserves the right to suspend or terminate user accounts at any time, with or without notice, for conduct that violates these Terms of Service, disrupts the platform, or is otherwise deemed harmful or inappropriate.
+                        FitClue reserves the right to suspend or terminate your accounts at any time, with or without notice, for conduct that violates these Terms of Service, disrupts the platform, or is otherwise deemed harmful or inappropriate.
                     </p>
                     <p>
-                        Users may also choose to stop using the platform at any time. Termination may result in the deletion of all associated user data, including account credentials.
+                        You may also choose to stop using the platform at any time. Termination may result in the deletion of all associated user data, including account credentials.
                     </p>
                     <p>
                         However, user-generated content, such as comments and posted links, may remain visible on the platform after account deletion, and the associated account identifiers may be removed or anonymized (e.g., replaced with "Deleted User"). FitClue is under no obligation to remove such content unless required by law.
@@ -179,7 +179,7 @@ const TOSData = () => {
             title: 'Terms Updates',
             description: (
                 <p>
-                    These Terms of Service may be updated at any time without prior notice. By continuing to use FitClue after changes are made, users agree to the revised terms. It is the user's responsibility to review the Terms periodically to remain informed.
+                    These Terms of Service may be updated at any time without prior notice. By continuing to use FitClue after changes are made, you agree to the revised terms. It is your responsibility to review the Terms periodically to remain informed.
                 </p>
             )
         }
