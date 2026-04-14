9bce98806bfffda60e14aefaf5db105c5c79953e
marditds <marditds@gmail.com>
2026-04-14 11:39:58 -0700

    Update main.js

--- functions/scanlink/src/main.js
+++ functions/scanlink/src/main.js
@@ -10,7 +10,7 @@ export default async ({ req, res, log, error }) => {
 
     log(body)
 
-    const rawLink = body?.body;
+    const rawLink = body?.link;
 
     if (!rawLink) {
       return res.json({
