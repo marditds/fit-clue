3ee6568765f361081ae2714b1860ed0ebd243b15
marditds <marditds@gmail.com>
2026-04-14 00:46:59 -0700

    Update main.js

--- functions/scanlink/src/main.js
+++ functions/scanlink/src/main.js
@@ -192,7 +192,7 @@ export default async ({ req, res, log, error }) => {
       verdict = 'not_valid_shopping_link';
     }
 
-    log(res)
+    log(verdict)
 
     return res.json({
       success: true,
