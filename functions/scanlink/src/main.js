16dc43e4b5b25cf7ee8928437125f7daa7dab788
marditds <marditds@gmail.com>
2026-04-24 01:03:00 -0700

    Update main.js

--- functions/scanlink/src/main.js
+++ functions/scanlink/src/main.js
@@ -105,7 +105,7 @@ const CONFIG = {
   // -------------------------
   // 🟢 Platform suppression
   // -------------------------
-  trustedBoostDomains = new Set([
+  trustedBoostDomains: new Set([
     'amazon.com',
     'ebay.com',
     'etsy.com',
