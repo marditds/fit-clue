d99280e6a97213a855f29221ed514e18a92227d2
marditds <marditds@gmail.com>
2026-08-04 23:41:59 -0700

    Update TopContributors.jsx

--- src/components/Home/TopContributors.jsx
+++ src/components/Home/TopContributors.jsx
@@ -45,21 +45,21 @@ const TopContributors = () => {
         </Container>);
 
     return (
-        <Container className='mb-5 pb-5 border main-border-radius'>
+        <Container className='mb-5 px-sm-4 py-3 border main-border-radius'>
             <Row className='align-items-center'>
-                <Col md={3}>
-                    <h5 className='text-uppercase secondary-text-color mb-2 mb-md-0 text-left'>
+                <Col>
+                    <h5 className='text-uppercase secondary-text-color text-left mb-3'>
                         Top contributors
                     </h5>
                 </Col>
             </Row>
             <Row>
                 <Col>
-                    <ol className='d-flex flex-wrap align-items-center justify-content-start mb-0 list-unstyled'>
+                    <ol className='d-flex flex-wrap align-items-center justify-content-start mb-0 list-unstyled gap-3'>
                         {
                             topContributors.map(((contributor, idx) => {
                                 return (
-                                    <li key={idx} className='px-2 py-1 px-sm-4 py-sm-2 mb-2 me-2 m-md-2 ms-md-0 border main-border-radius fw-bold secondary-bg-color'>
+                                    <li key={idx} className='px-2 py-1 border main-border-radius fw-bold secondary-bg-color'>
                                         <span className='tertiary-text-color secondary-font-size'>{idx + 1}</span>
                                         <span className='px-3'>{contributor.username}</span>
                                         <span className='secondary-text-color'>{contributor.score}</span>
